b93ce02198d3d39b646cf19c8ec71644e45866a9
Soham Dasgupta <sohamdasg@gmail.com>
2020-05-17 18:11:45 -0500

    Sphere hit-check

--- main.cc
+++ main.cc
@@ -4,30 +4,38 @@
 
 #include <iostream>
 
-bool hit_sphere(const point3& center, float radius, const ray& r) {
+float hit_sphere(const point3& center, float radius, const ray& r) {
     //Solving quadratic equation
     //0 roots -> ray missed sphere
     //1 root -> ray runs tangent to sphere
     //2 roots -> ray enters and exits sphere
     vec3 oc = r.origin() - center;
     auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-    return (discriminant > 0);
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius*radius;
+    auto discriminant = half_b*half_b - 4*a*c;
+
+    return (discriminant < 0) ? -1.0
+    : (-half_b - sqrt(discriminant)) / a;
 }
+
 color ray_color(const ray& r) {
-    if (hit_sphere(point3(0,0,-1), 0.5, r))
-        return color(1,0,0);
+    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
+    //If ray hits sphere, render sphere
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
+    //Else, render background
     vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5*(unit_direction.y() + 1);
+    t = 0.5*(unit_direction.y() + 1.0);
     // linear interpolation from white to blue:  (1-t)*'blue' + (t)*'white'
     return (1.0-t)*color(1.0,1.0,1.0) + t*color(0.5,0.7,1.0);
 }
 
 int main() {
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 384;
+    const int image_width = 720;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
